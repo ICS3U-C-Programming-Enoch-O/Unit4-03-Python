31acd93ee352539696bb281103f742d40ff33b4a
Enoch Oyeniyi <enoch.oyeniyi@stu.ocsb.ca>
2025-05-05 16:30:26 +0000

    “power_of_two”

---
+++ .github/power_of_two.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Created by: Enoch O
+# Created on: April 16, 2025
+# A loop for to calculate the square of every number leading up to the user's input
+def main():
+
+# Get Users Input
+    try:
